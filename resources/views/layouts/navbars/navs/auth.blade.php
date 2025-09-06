51998a027e94d3de8d29be961da0a629728bb8e8
herbertzap <herbert.zapata19@gmail.com>
2025-09-06 19:51:14 +0000

    🔧 Corregir error de logout en navegación
    - Cambiado href de logout de route('logout') a '#'
    - El logout se maneja correctamente con JavaScript
    - Evita error 405 Method Not Allowed
    - Formulario de logout POST funciona correctamente
    - Navegación de logout ahora funciona sin errores

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -56,7 +56,7 @@
                         </li>
                         <li class="dropdown-divider"></li>
                         <li class="nav-link">
-                            <a href="{{ route('logout') }}" class="nav-item dropdown-item" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
+                            <a href="#" class="nav-item dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
                         </li>
                     </ul>
                 </li>>
